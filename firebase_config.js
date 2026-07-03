dceb7a95af100d43b34a5905c1373659600cf959
phucclerkly12 <phuclb3@fe.edu.vn>
2026-07-03 11:03:05 +0700

    firebase

---
+++ firebase_config.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbH7RauamjhU-eT4nymGe_2QQ_xGrZIus",
+  authDomain: "game-9c1b4.firebaseapp.com",
+  projectId: "game-9c1b4",
+  storageBucket: "game-9c1b4.firebasestorage.app",
+  messagingSenderId: "553539320501",
+  appId: "1:553539320501:web:8dd2be4ac858eb6be01cfc",
+  measurementId: "G-TG1LEC5MPE"
+};
